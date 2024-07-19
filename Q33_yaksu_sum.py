945fdf7a6a35ca3816ab5c0598aa5fdd474c129e
Jannys <slalstkd45@gmail.com>
2024-07-19 12:37:26 +0900

    add description all question

--- Q33_yaksu_sum.py
+++ Q33_yaksu_sum.py
@@ -1,15 +1,23 @@
+# 두 정수 left와 right가 매개변수로 주어집니다.
+# left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고,
+# 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# 1 ≤ left ≤ right ≤ 1,000
+
 def solution(left, right):
     answer = 0
-    for i in range(left,right+1):
+    for i in range(left, right+1):
         length = []
-        for j in range(1,i+1):
+        for j in range(1, i+1):
             if i % j == 0:
-                length.append(j)   
-                
+                length.append(j)
+
         if len(length) % 2 == 0:
             answer += i
         elif len(length) % 2 == 1:
             answer -= i
     return answer
 
-print(solution(13,17))
+
+print(solution(13, 17))
--- Q34_str_nerimchasun.py
+++ Q34_str_nerimchasun.py
@@ -1,3 +1,5 @@
+
+
 def solution(s):
     answer = ''
     answer = sorted(s)
