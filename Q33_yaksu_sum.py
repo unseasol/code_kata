b2d52182a7796222c4ef63ef2a8855f28f94024e
Jannys <slalstkd45@gmail.com>
2024-07-19 10:59:39 +0900

    write code ~ Q34

---
+++ Q33_yaksu_sum.py
@@ -0,0 +1,15 @@
+def solution(left, right):
+    answer = 0
+    for i in range(left,right+1):
+        length = []
+        for j in range(1,i+1):
+            if i % j == 0:
+                length.append(j)   
+                
+        if len(length) % 2 == 0:
+            answer += i
+        elif len(length) % 2 == 1:
+            answer -= i
+    return answer
+
+print(solution(13,15))
---
+++ Q34_str_nerimchasun.py
@@ -0,0 +1,6 @@
+def solution(s):
+    answer = ''
+    answer = sorted(s)
+    answer.reverse()
+    answer = "".join(answer) # Join : 문자열을 구분자 앞의 형식으로 합쳐라
+    return answer
